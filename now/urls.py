3f30327b7b922d81b6ae286820a9f1fe3f24bf22
yobgith <yobouet.yao@gmail.com>
2024-06-03 11:49:23 +0000

    store and carts functionality

--- now/urls.py
+++ now/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.accueil, name='accueil'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
